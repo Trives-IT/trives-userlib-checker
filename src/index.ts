fadd8dbaf4430d803966ab1de67f665cf32f2b37
WiemerSteenbeek <wiemer.steenbeek@gmail.com>
2023-09-17 17:18:15 +0200

    First draft

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as fs from "fs";
 import { IModel } from "mendixmodelsdk";
 
 const config = require("../config/config.json");
+const libnames = require("../config/libnames.json");
 
 let mxClient: MendixPlatformClient;
 
@@ -12,25 +13,45 @@ async function main() {
   // Set up your Mendix Model SDK client:
   setPlatformConfig({ mendixToken: config.mendixtoken });
   mxClient = new MendixPlatformClient();
-
-  // Option 1: Get an existing app:
   const mxApp = getExistingApp(config.projectid);
-
-  // Option 2: Create a new app:
-  // const mxApp = await createNewApp("testje", undefined, "git");
-
-  // Create a working copy and open the model:
   const mxWorkingCopy = await mxApp.createTemporaryWorkingCopy(config.branch);
   const mxModel = await mxWorkingCopy.openModel();
 
-  // Do your worst here :-): Some examples:
-  console.log(mxApp.appId);
-  console.log(mxWorkingCopy.workingCopyId);
-  console.log(mxModel.allModules().forEach((module) => console.log(module.name)));
+  const libraryNames: string[] = JSON.parse(JSON.stringify(libnames)); // Load the list of library names from the config file
 
-  // Optional, if you want to commit your changes:
-  // await mxModel.flushChanges();
-  // await mxWorkingCopy.commitToRepository(config.branch);
+  // Step 1: Get all the files in the project that are .jar files and are in the userlib folder
+  const files = (await mxModel.getFiles()).filter((file) => file.endsWith(".jar")).filter((file) => file.includes("userlib"));
+
+  // Step 2: Loop through the list of library names and count how many times each library name is found in the list of files. Output to a map.
+  const libraryCountMap = new Map<string, number>(); // Prepare a map to store the result counts
+  for (const word of libraryNames) {
+    let count = 0;
+    for (const fileName of files) {
+      if (fileName.includes(word)) {
+        count++;
+      }
+    }
+    libraryCountMap.set(word, count);
+  }
+
+  // Step 3: Filter the libraryCountMap to only include libraries that are found more than once
+  const filteredResults = new Map<string, number>();
+  libraryCountMap.forEach((count, word) => {
+    if (count > 1) {
+      filteredResults.set(word, count);
+    }
+  });
+
+  // Step 4: Get the files that match the filteredResults
+  const outputFiles: string[][] = [];
+  for (const filteredResult of filteredResults) {
+    const file = (await mxModel.getFiles()).filter((file) => file.endsWith(".jar")).filter((file) => file.includes(filteredResult[0]));
+    outputFiles.push(file);
+  }
+
+  // Step 5: Write the output to a JSON file
+  fs.rmSync("output/output.json", { recursive: true, force: true });
+  fs.writeFileSync("output/output.json", JSON.stringify(outputFiles));
 }
 
 async function createNewApp(name: string, templateId?: string, repositoryType: RepositoryType = "git"): Promise<App> {
