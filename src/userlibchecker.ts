ab646fed592cd5ca8d36026a103f6fa1a756ef57
WiemerSteenbeek <wiemer.steenbeek@gmail.com>
2023-10-08 14:41:54 +0200

    Formatting

--- src/userlibchecker.ts
+++ src/userlibchecker.ts
@@ -1,4 +1,4 @@
-import { App, MendixPlatformClient, RepositoryType, setPlatformConfig } from "mendixplatformsdk";
+import { MendixPlatformClient, setPlatformConfig } from "mendixplatformsdk";
 import * as fs from "fs";
 
 const config = require("../config/config.json");
@@ -7,65 +7,52 @@ let mxClient: MendixPlatformClient;
 main().catch(console.error);
 
 async function main() {
-  // Set up your Mendix Model SDK client:
+
+  // Step 1: Set up your Mendix Model SDK client and open your project:
   setPlatformConfig({ mendixToken: config.mendixtoken });
   mxClient = new MendixPlatformClient();
-  const mxApp = getExistingApp(config.projectid);
+  const mxApp = mxClient.getApp(config.projectid);
   const mxWorkingCopy = await mxApp.createTemporaryWorkingCopy(config.branch);
   const mxModel = await mxWorkingCopy.openModel();
 
-  // Step 1: Get all the files in the project that are .jar files and are in the userlib folder
-  const files = (await mxModel.getFiles()).filter((file) => file.endsWith(".jar")).filter((file) => file.includes("userlib"));
+  // Step 2: Get all the files that are .jar files and in userlib folder
+  const jarFiles = (await mxModel.getFiles())
+    .filter((file) => file.endsWith(".jar"))
+    .filter((file) => file.startsWith("userlib"));
 
-  // Step 2: Create an empty list of library collections
-  let libraryList: Library[] = [];
+  // Step 3: For each file, get the library name and version. If both are found, add it to the library list
+  let libraryList: {
+    libraryName: string;
+    libraryVersions: { libraryVersion: string; fullFileName: string }[];
+  }[] = [];
 
-  // Step 3: Loop through the list of files and get the library name and version for each file. If both are found, add it to the library list
-  for (const file of files) {
+  for (const file of jarFiles) {
     try {
-      const fileNameProper = file.split("/")[1]; // Get the filename after the userlib folder
+      const fileNameProper = file.split("/")[1];
       // prettier-ignore
-      const versionRegex = new RegExp("^[0-9]+(\.[0-9]+)+"); // The regex covers all versions that are in the format x.x, x.x.x etc.
-      const version = fileNameProper
-        .split(".jar")[0] // Remove the .jar extension
-        .split("-") // Split the filename on the - character
-        .find((snippet) => versionRegex.test(snippet))!; // Find the part that matches the regex for the version
-      const baseLibraryName = fileNameProper.substring(0, fileNameProper.indexOf(version) - 1); // Get the base library name without the version
-      const libraryVersion = { libraryVersion: version, fullFileName: fileNameProper }; // Create a library version
-      const library = getLibrary(libraryList, baseLibraryName); // Get the library object from the library list or add new one if it doesn't exist
-      library.libraryVersions.push(libraryVersion); // Add the library version to the library object
+      const version = fileNameProper 
+        .split(".jar")[0] .split("-") 
+        .find((snippet) => new RegExp("^[0-9]+(\.[0-9]+)+").test(snippet))!;
+      const baseLibraryName = fileNameProper.substring(0, fileNameProper.indexOf(version) - 1);
+      const libraryVersion = { libraryVersion: version, fullFileName: fileNameProper };
+      const library = libraryList.find((library) => library.libraryName === baseLibraryName) || 
+      {libraryName: baseLibraryName, libraryVersions: []};
+      if (!libraryList.find((library) => library.libraryName === baseLibraryName)) libraryList.push(library);
+      library.libraryVersions.push(libraryVersion);
     } catch {
-      console.warn("Failed to get library name and version for file: " + file + ". It does not adhere to the usual naming convention.");
+      console.warn("Failed to get library name and version for file: " + file);
     }
   }
 
-  fs.writeFileSync("output/output.json", JSON.stringify(libraryList.sort((a, b) => a.libraryName.localeCompare(b.libraryName)).filter((library) => library.libraryVersions.length > 1)));
-  // Write the library list (with more than 1 version & sorted on alphabet by library name) to a json file
+  // Step 4: Write the library list to a json file if it contains more than 1 version
+  fs.writeFileSync(
+    "output/output.json",
+    JSON.stringify(
+      libraryList
+        .sort((a, b) => a.libraryName.localeCompare(b.libraryName))
+        .filter((library) => library.libraryVersions.length > 1)
+    )
+  );
+
   console.log("Done! Java library list succesfully written to output/output.json");
-}
-
-async function createNewApp(name: string, templateId?: string, repositoryType: RepositoryType = "git"): Promise<App> {
-  const newApp = await mxClient.createNewApp(name, { templateId: templateId, repositoryType: repositoryType });
-  return newApp;
-}
-
-function getExistingApp(projectId: string): App {
-  return mxClient.getApp(projectId);
-}
-
-function getLibrary(libraryList: Library[], libraryName: string): Library {
-  const library = libraryList.find((library) => library.libraryName === libraryName);
-
-  if (library) {
-    return library;
-  } else {
-    const library: Library = { libraryName: libraryName, libraryVersions: [] };
-    libraryList.push(library);
-    return library;
-  }
-}
-
-interface Library {
-  libraryName: string;
-  libraryVersions: { libraryVersion: string; fullFileName: string }[];
 }
